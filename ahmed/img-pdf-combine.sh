18d36497f24971b2c537f5eab73baaf896d82987
Ahmed Mohamed <am0103738@gmail.com>
2023-06-07 20:31:44 +0300

    added images to pdf

---
+++ ahmed/img-pdf-combine.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# convert folder of images to pdf or files 
+
+IFS=$'\n'
+selected_files=($NAUTILUS_SCRIPT_SELECTED_FILE_PATHS)
+unset IFS
+
+if [ -z "$selected_files" ]; then
+    zenity --error --text="No files selected"
+    exit 1
+fi
+
+# if the selected is a folder 
+if [ -d "${selected_files[0]}" ]; then
+    selected_files=("${selected_files[0]}"/*)
+fi
+
+# get the directory of the first selected file
+output_dir=$(dirname "${selected_files[0]}")
+
+# convert images to pdf
+convert "${selected_files[@]}" "${output_dir}/converted_combined.pdf"
